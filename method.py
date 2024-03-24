627fa17a33f77ea70b66d780dd23eddf07f46523
discphy <dinophy@nate.com>
2024-03-25 08:29:30 +0900

    feat(add): method added

---
+++ method.py
@@ -0,0 +1,15 @@
+name = "discphy"
+
+print(name.capitalize())
+
+days_of_week = ["Mon", "Tue", "Wed", "Thu", "Fri"]
+
+print(days_of_week[2])
+
+days = ("Mon", "Tue", "Wed")
+
+player = {
+    'name': 'discphy',
+    'age': 12,
+    'alive': True
+}
