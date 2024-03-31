a3f869dd76835a3812637a1d876dc2daff652b45
discphy <dinophy@nate.com>
2024-03-31 12:14:35 +0900

    feat(add): classes

--- Classes.py
+++ Classes.py
@@ -1,14 +1,34 @@
-class Puppy:
+class Dog:
+
+    def __init__(self, name, breed, age):
+        self.name = name
+        self.breed = breed
+        self.age = age
+
+    def sleep(self):
+        print("Zzz")
+
+class GuardDog(Dog):
 
     def __init__(self, name, breed):
-        self.name = name
-        self.age = 0.1
-        self.breed = breed
+        super().__init__(name, breed, 5)
+        self.aggressive = True
 
-    def __str__(self):
-        return f"{self.breed} puppy named {self.name}"
+    def rrrrr(self):
+        print("stay away!")
+
+class Puppy(Dog):
+
+    def __init__(self, name, breed):
+        super().__init__(name, breed, 0.1)
+        self.spoiled = True
+
+    def woof_woof(self):
+        print("Woof Woof!")
+
 
 ruffus = Puppy(name="Ruffus", breed="Biggle")
-bibi = Puppy(name="Bibi", breed="Dalmatian")
+bibi = GuardDog(name="Bibi", breed="Dalmatian")
 
-print(ruffus, bibi)
+ruffus.sleep()
+bibi.sleep()
